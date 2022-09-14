8c8f6b8bef4259ecccd0cbf574e8a1e18aa74562
Mike Claros <mike.s.claros@gmail.com>
2022-09-14 14:16:23 -0700

    added next prev functionality

--- src/Client/components/Card.jsx
+++ src/Client/components/Card.jsx
@@ -4,15 +4,36 @@ import apis from '../api';
 
 export function Card({ value }) {
     const [cards, setCards] = useState([])
-    useEffect(() => { handleValue(value) })
+    const [curIndex, setCurIndex] = useState(-1)
+    useEffect(() => { handleValue(value) }, [value])
 
 
-    function handleValue(value) {
-        setCards(() => value)
+    function handleValue(data) {
+        setCards(() => data)
+        let randIndex = Math.floor(Math.random() * data.length) //random number from range max min: (max -min) + min -> (data.length -0) - 0
+        setCurIndex(() => randIndex)
     }
 
-    function randomQuestion() {
-        return cards[Math.floor(Math.random() * cards.length)].question //random number from range max min: (max -min) + min -> (data.length -0) - 0
+    const handleNext = (e) => {
+        e.preventDefault()
+        setCurIndex(() => {
+            if (curIndex + 1 < cards.length) {
+                return curIndex + 1
+            } else {
+                return 0
+            }
+        })
+    }
+
+    const handlePrev = (e) => {
+        e.preventDefault()
+        setCurIndex(() => {
+            if (curIndex - 1 < 0) {
+                return cards.length - 1
+            } else {
+                return curIndex - 1
+            }
+        })
     }
 
     return (
@@ -20,16 +41,17 @@ export function Card({ value }) {
             <div className='card-display'>
                 <div>
                     <div>
-                        {(cards.length > 0 && cards != undefined) ? randomQuestion() : console.log('Error!', cards)}
+                        {console.log('index ?', curIndex)}
+                        {(cards.length > 0 && cards != undefined) ? cards[curIndex].question : ''}
                     </div>
                 </div>
             </div>
 
             <div className='padding'>
                 <form className='form'>
-                    <button id="prev">Prev</button>
+                    <button id="prev" onClick={(e) => handlePrev(e)}>Prev</button>
                     <button id="submit">Submit</button>
-                    <button id="next">Next</button>
+                    <button id="next" onClick={(e) => handleNext(e)}>Next</button>
                 </form>
             </div>
 
